d3c40629e1b0aa794a6eb8ac496684a3d5ceeadd
AlexDjangoX <alexmonk17@gmail.com>
2022-05-05 15:19:43 +0200

    Basic Criteria met

--- src/App.js
+++ src/App.js
@@ -1,60 +1,87 @@
-import "./App.css"
+import "./App.css";
+import { useState, useReducer } from "react";
 
-function App() { 
+function App() {
+  const [leftNumber, setLeftNumber] = useState("0");
+  const [rightNumber, setRightNumber] = useState("0");
+  const [symbol, setSymbol] = useState("+");
+  const [calculatedTotal, setCalculatedTotal] = useState("0");
+
+  const totalCalculatedValue = () => {
+    if (symbol === "+") {
+      setCalculatedTotal(Number(leftNumber) + Number(rightNumber));
+    }
+    if (symbol === "-") {
+      setCalculatedTotal(Number(leftNumber) - Number(rightNumber));
+    }
+    if (symbol === "*") {
+      setCalculatedTotal(Number(leftNumber) * Number(rightNumber));
+    }
+    if (symbol === "÷") {
+      setCalculatedTotal(Number(leftNumber) / Number(rightNumber));
+    }
+    setLeftNumber("0");
+    setRightNumber("0");
+  };
+
+  const setNumberOne = () => {
+    setLeftNumber("");
+    setLeftNumber(leftNumber + "1");
+  };
 
   return (
     <div className="calculator">
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{leftNumber}</p>
+        <div className="numbers">
+          <button onClick={() => setNumberOne()}>1</button>
+          <button onClick={() => setLeftNumber(leftNumber + "2")}>2</button>
+          <button onClick={() => setLeftNumber(leftNumber + "3")}>3</button>
+          <button onClick={() => setLeftNumber(leftNumber + "4")}>4</button>
+          <button onClick={() => setLeftNumber(leftNumber + "5")}>5</button>
+          <button onClick={() => setLeftNumber(leftNumber + "6")}>6</button>
+          <button onClick={() => setLeftNumber(leftNumber + "7")}>7</button>
+          <button onClick={() => setLeftNumber(leftNumber + "8")}>8</button>
+          <button onClick={() => setLeftNumber(leftNumber + "9")}>9</button>
+          <button onClick={() => setLeftNumber(leftNumber + "0")}>0</button>
+          <button onClick={() => setLeftNumber("0")}>Clear</button>
         </div>
-        
-        <div className="panel">
-          <p>+</p>
-          <div className="numbers">
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>÷</button>
-          </div>
-        </div>
+      </div>
 
-        <div className="panel">
-          <p>0</p>
-          <div className="numbers">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-            <button>Clear</button>
-          </div>
+      <div className="panel">
+        <p>{symbol}</p>
+        <div className="numbers">
+          <button onClick={() => setSymbol("+")}>+</button>
+          <button onClick={() => setSymbol("-")}>-</button>
+          <button onClick={() => setSymbol("*")}>*</button>
+          <button onClick={() => setSymbol("÷")}>÷</button>
         </div>
-        <div className="panel answer">
-          <p>0</p>
-          <div>
-            <button>=</button>
-          </div>
+      </div>
+
+      <div className="panel">
+        <p>{rightNumber}</p>
+        <div className="numbers">
+          <button onClick={() => setRightNumber(rightNumber + "1")}>1</button>
+          <button onClick={() => setRightNumber(rightNumber + "2")}>2</button>
+          <button onClick={() => setRightNumber(rightNumber + "3")}>3</button>
+          <button onClick={() => setRightNumber(rightNumber + "4")}>4</button>
+          <button onClick={() => setRightNumber(rightNumber + "5")}>5</button>
+          <button onClick={() => setRightNumber(rightNumber + "6")}>6</button>
+          <button onClick={() => setRightNumber(rightNumber + "7")}>7</button>
+          <button onClick={() => setRightNumber(rightNumber + "8")}>8</button>
+          <button onClick={() => setRightNumber(rightNumber + "9")}>9</button>
+          <button onClick={() => setRightNumber(rightNumber + "0")}>0</button>
+          <button onClick={() => setRightNumber("0")}>Clear</button>
         </div>
+      </div>
+      <div className="panel answer">
+        <p>{calculatedTotal}</p>
+        <div>
+          <button onClick={() => totalCalculatedValue()}>=</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
